c8c478ee2f783d7028ccd34f1e641758199539a2
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2025-02-15 22:38:46 -0300

    Correções chat

--- API/service/chatService.js
+++ API/service/chatService.js
@@ -5,9 +5,6 @@ module.exports = {
 
         const verify_token = process.env.VERIFY_TOKEN;
 
-        console.log(req.query)
-        console.log(verify_token)
-
         // Parse params from the webhook verification request
         let mode = req.query["hub.mode"];
         let token = req.query["hub.verify_token"];
@@ -31,13 +28,20 @@ module.exports = {
 
     async webHook(req, res) {
         const body = req.body.entry[0];
-        console.log(body);
-        if(body.field !== 'messages'){
+        if(body.changes[0].field !== 'messages'){
             // not from the messages webhook so dont process
-            return res.sendStatus(400);
+            return res.status(400).json({boddy: body});
         };
-        return res.sendStatus(200);
+
+        const message = body.changes[0].value.messages[0] || null;
+
+        console.log(body.changes[0].value)
+        console.log(message)
+        if(message === null){
+            return res.status(400).json({body: body});
+        } else {
+            return res.status(200).json({message: message});
+        }
     }
 
 }
-
