84928783a5bb669bb8aae815ca3263bdbd83bef7
Luizfernando5212 <107008441+Luizfernando5212@users.noreply.github.com>
2025-02-18 14:14:37 -0300

    Update chatService.js

--- API/service/chatService.js
+++ API/service/chatService.js
@@ -33,7 +33,8 @@ export default {
 
     async webHook(req, res) {
         const body = req.body.entry[0];
-        if (body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.timestamp < Date.now() / 1000 - 10 || ) {
+        if (body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.timestamp < Date.now() / 1000 - 10 || 
+           !body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]) {
             console.log('ignoring webhook, mensagem antiga');
             return res.status(200).json({ message: 'ok' });
         } else {
