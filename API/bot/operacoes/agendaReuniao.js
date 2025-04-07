57ebaca32a0385e287c48dbc331d68078a0dcd83
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2025-04-07 08:48:40 -0300

    Funcionalidade de agendar reunião por setor

--- API/bot/operacoes/agendaReuniao.js
+++ API/bot/operacoes/agendaReuniao.js
@@ -6,6 +6,32 @@ import { textMessage } from '../../utll/requestBuilder.js';
 import axios from 'axios';
 import mongoose from 'mongoose';
 
+const adicionaParticipante = async (participante, reuniao) => {
+    const telefone = await Telefone.findOne({ pessoa: participante._id });
+    if (!telefone) {
+        console.log('Telefone não encontrado para a pessoa:', participante._id);
+        return;
+    }
+    if (!reuniao || !reuniao._id) {
+        console.log('Erro: reuniao não encontrada ou sem _id');
+        return;
+    }
+    const participanteId = new mongoose.Types.ObjectId(participante._id);
+    const reuniaoId = new mongoose.Types.ObjectId(reuniao._id);
+    const novoParticipante = {
+        pessoa: participanteId,
+        reuniao: reuniaoId,
+        conviteAceito: true
+    }
+    const participanteDoc = new Participantes(novoParticipante);
+    await participanteDoc.save();
+
+    const mensagem = `Olá ${participante.nome}, você foi convidado para a reunião ${reuniao.titulo} no dia ${reuniao.dataHoraInicio}.`;
+    if (reuniao.organizador !== participante._id) {
+        await axios(textMessage(telefone.numero, mensagem));
+    }
+}
+
 /**
  * 
  * @param {Object} consulta - Objeto de consulta de pessoa retornado do banco de dados
@@ -14,51 +40,35 @@ import mongoose from 'mongoose';
  * @returns 
  */
 const agendaReuniao = async (consulta, objReuniao, res) => {
-    objReuniao.organizador = consulta[0]._id;
+    objReuniao.organizador = consulta[0].pessoa._id;
     const novaReuniao = await Reuniao.create(objReuniao);
-
-    const participantes = objReuniao.participantes;
+    let participantes = objReuniao.participantes;
+    const setor = objReuniao.setor;
 
     try {
-        for (let i = 0; i < participantes.length; i++) {
-            let participante = await Pessoa.find({ nome: participantes[i] });
-            if (participante.length === 1) {
-                const telefone = await Telefone.findOne({ pessoa: participante[0]._id });
-
-                if (!telefone) {
-                    console.log('Telefone não encontrado para a pessoa:', participante[0]._id);
-                    return;
-                }
-
-                if (!novaReuniao || !novaReuniao._id) {
-                    console.log('Erro: novaReuniao não encontrada ou sem _id');
-                    return;
-                }
-
-                const participanteId = new mongoose.Types.ObjectId(participante[0]._id);
-                const reuniaoId = new mongoose.Types.ObjectId(novaReuniao._id);
-
-                const novoParticipante = {
-                    pessoa: participanteId,
-                    reuniao: reuniaoId,
-                    conviteAceito: true
-                }
-
-
-                const participateDoc = new Participantes(novoParticipante);
-                await participateDoc.save();
-                const mensagem = `Olá ${participante[0].nome}, você foi convidado para a reunião ${novaReuniao.titulo} no dia ${novaReuniao.dataHoraInicio}.`;
-                await axios(textMessage(telefone.numero, mensagem));
-            } else if (participante.length > 1) {
-                // Enviar lista de pessoas com o mesmo nome para o usuário escolher
-            } else {
-                // Enviar mensagem para o usuário informando que não foi encontrado
+        if (participantes.length === 0 && setor !== null) {
+            participantes = await Pessoa.find({ setor: setor });
+            if (participantes.length === 0) {
+                console.log('Nenhum participante encontrado para o setor:', setor);
+                return;
             }
+            participantes.forEach(async (participante) => {
+                await adicionaParticipante(participante, novaReuniao);
+            });
+        } else {
+            participantes.forEach(async (participante) => {
+                let participanteDoc = await Pessoa.find({ nome: { $regex: `^${participante}`, $options: 'i' } });
+                if (participanteDoc.length === 1) {
+                    await adicionaParticipante(participanteDoc[0], novaReuniao);
+                } else if (participanteDoc.length > 1) {
+                    // Enviar lista de pessoas com o mesmo nome para o usuário escolher
+                } else {
+                    // Enviar mensagem para o usuário informando que não foi encontrado
+                }
+            });
+            await adicionaParticipante(consulta[0].pessoa, novaReuniao);
         }
 
-        const participateDoc = new Participantes(novoParticipante);
-        await participateDoc.save();
-        await axios(textMessage(consulta[0].numero, 'Reunião agendada com sucesso!'));
         return res.status(200).json({ message: 'Reunião agendada com sucesso!' });
     } catch (error) {
         console.log(error);
--- API/bot/text/mensagemTexto.js
+++ API/bot/text/mensagemTexto.js
@@ -5,6 +5,7 @@ import OpenAI from 'openai';
 import z from 'zod';
 import dotenv from 'dotenv';
 import agendaReuniao from "../operacoes/agendaReuniao.js";
+import Setor from "../../model/setor.js";
 
 dotenv.config();
 
@@ -16,10 +17,11 @@ const Evento = z.object({
     local: z.string().optional(),
     pauta: z.string().optional(),
     dataHoraInicio: z.string().describe('formato ISO 8601 (YYYY-MM-DDTHH:MM:SSZ)'),
-    dataHoraFim: z.string().describe('formato ISO 8601 (YYYY-MM-DDTHH:MM:SSZ)'),
+    dataHoraFim: z.string().describe('formato ISO 8601 (YYYY-MM-DDTHH:MM:SSZ), apenas preencha caso esteja explícita.'),
     setor: z.string().optional().describe('Setor só deve ser preenchido caso não encontre participantes'),
     participantes: z.array(z.string()),
-    isDentroTema: z.boolean().describe('Deve ser true para ser considerado uma reunião válida.')
+    isDentroTema: z.boolean().describe('Deve ser true para ser considerado uma reunião válida.'),
+    isSuficiente: z.boolean().describe('Deve ser true caso possua informações suficientes para agendar a reunião (titulo, dataHoraInicio, dataHoraFim, participantes).'),
 });
 
 /**
@@ -33,26 +35,43 @@ async function estruturaMensagemTexto(texto) {
         const reuniao = await openai.beta.chat.completions.parse({
             model: 'gpt-4o-mini-2024-07-18',
             messages: [
-                { role: 'system', content: 'Extraia as informações do evento/reunião, não produza informações, hoje é dia ' + new Date() + '. Caso não haja informações o suficiente, preencha apenas o campo isDentroTema com false' },
+                { role: 'system', content: 'Extraia as informações do evento/reunião, não produza informações, hoje é dia ' + new Date() +
+                    ' E a reunião deve ser agendada para o futuro, não produza informações que não estejam explícitas no texto.' +
+                    ' titulo, dataHoraInicio, dataHoraFim, (participantes ou setor) são informações obrigatórias.' +
+                    ' Você deve saber diferencias um setor de uma pessoa, o setor é o nome do departamento e a pessoa é o nome/apelido do funcionário.' +
+                    ' isDentroTema diz se a messagem é sobre reunião ou não, isSuficiente diz se possui informações suficientes para agendar a reunião.' },
                 { role: 'user', content: texto },
             ],
             response_format: responseFormat,
         });
         let resultado = reuniao.choices[0].message.parsed;
+        console.log(resultado)
         if (resultado.isDentroTema) {
-            if (resultado.setor === '') {
-                resultado.setor = null;
-            }
-            return resultado;
-        } else {
-            let msg = 'Não identificamos algunas informações na sua mensagem. Por favor, informe: ';
-            const camposObrigatorios = new Set(['titulo', 'dataHoraInicio', 'dataHoraFim', 'participantes']);
-            Object.entries(resultado).forEach(([key, value]) => {
-                if (camposObrigatorios.has(key) && (value == null || value === '' || value.length === 0)) {
-                    msg += key  + ', ';
+            if (resultado.isSuficiente) {
+                if (resultado.setor === '') {
+                    resultado.setor = null;
+                    return resultado;
+                } else {
+                    // Trecho ficará responsável por buscar o setor no banco de dados
+                    // e substituir o valor do setor no objeto resultado
+                    const setor = await Setor.find({ descricao: resultado.setor });
+                    resultado.setor = setor[0]._id;
+                    return resultado;
                 }
-            });    
-            
+            } else {
+                let msg = 'Não conseguimos identificar todas as informações necessárias para agendar a reunião. Por favor, informe: ';
+                const camposObrigatorios = new Set(['titulo', 'dataHoraInicio', 'dataHoraFim', 'participantes']);
+                const camposInvalidos = [];
+                Object.entries(resultado).forEach(([key, value]) => {
+                    if (camposObrigatorios.has(key) && (value == null || value === '' || value.length === 0)) {
+                        camposInvalidos.push(key);
+                    }
+                });
+                msg += camposInvalidos.join(', ');
+                return msg;
+            }
+        } else {
+            let msg = 'O VoxBot foi criado para agendar reuniões, por favor utilize-o para isso.';
             return msg;
         }
     } catch (err) {
@@ -62,23 +81,23 @@ async function estruturaMensagemTexto(texto) {
 }
 
 const mensagemTexto = async (consulta, numeroTel, mensagem, res) => {
-    const resposta =  await estruturaMensagemTexto(mensagem);
+    const resposta = await estruturaMensagemTexto(mensagem);
     if (typeof resposta === "object" && resposta !== null) {
         console.log("É um objeto");
-        const respostaString = JSON.stringify(resposta); 
+        const respostaString = JSON.stringify(resposta);
         try {
             await agendaReuniao(consulta, resposta, res);
-            await axios(textMessage(numeroTel, respostaString))
-            // res.status(200).json({ message: 'Message sent successfully' });
+            await axios(textMessage(numeroTel, respostaString));
         } catch (err) {
             console.log(err);
             res.status(400).json({ error: 'Error sending message' + err });
         }
     } else {
         await axios(textMessage(numeroTel, resposta));
+        res.status(200).json({ message: 'Message sent successfully' });
     }
-    
-    
+
+
 
 }
 
