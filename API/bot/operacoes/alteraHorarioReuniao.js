6edc28ed659c2fb85d3f00d075625dc7a07d5299
Ga-Rasquinho <gabriel_rasquinho@outlook.com>
2025-06-09 12:05:20 -0300

    Ajuste no nome da função.

--- API/bot/operacoes/alteraHorarioReuniao.js
+++ API/bot/operacoes/alteraHorarioReuniao.js
@@ -108,7 +108,7 @@ async function updateHorarioReuniaoMongoDB(resultado, numeroTel, consulta) {
         const duracaoReuniao = dayjs.utc(reuniao_encontrada.dataHoraFim).diff(dayjs.utc(reuniao_encontrada.dataHoraInicio), 'minute');
 
         if (resultado.novoHorarioFim && resultado.novoHorarioFim.trim() !== '') {
-            dates.novoHorarioFim = new Date(validaConversaoUTC(resultado.novoHorarioFim));
+            dates.novoHorarioFim = new Date(converteParaHorarioUTC(resultado.novoHorarioFim));
         } else {
             dates.novoHorarioFim = new Date(dates.novoHorarioInicio.getTime() + duracaoReuniao * 60000); // Adiciona a duração da reunião ao novo horário de início
         }
