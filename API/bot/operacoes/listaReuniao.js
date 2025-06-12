b75efcabbb471058626bfd2878f7fb1fbc5cdd08
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2025-06-12 15:41:39 -0300

    Mensagem de lista de reuniões

--- API/bot/operacoes/listaReuniao.js
+++ API/bot/operacoes/listaReuniao.js
@@ -204,7 +204,7 @@ async function formatarListaReunioes(reunioes) {
         console.log(r);
         const participantes = participantesPorReuniao[i];
         const horarioInicio = dayjs(r.dataHoraInicio).format("DD/MM/YYYY, [Inicia às] HH:mm");
-        const horarioFim = dayjs(r.dataHoraFim).format(", [Finaliza às] HH:mm");
+        const horarioFim = dayjs(r.dataHoraFim).format("[Finaliza às] HH:mm");
 
         mensagem += `*${i + 1}.* 📅 *Título:* ${r.titulo || "Sem título"}\n`;
         mensagem += `   🕒 *Data:* ${horarioInicio}, ${horarioFim}\n`;
